131ccda6026acab8835daaa62fe4aee140ac6fae
Jade Landeg <j-landeg@hotmail.co.uk>
2022-01-21 12:09:06 +0000

    login tests

---
+++ firebaseInitialise.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApH4p08BVpgDQeHcShHGCBOyUGL2koGBc",
+  authDomain: "spiking-log-in-ce622.firebaseapp.com",
+  projectId: "spiking-log-in-ce622",
+  storageBucket: "spiking-log-in-ce622.appspot.com",
+  messagingSenderId: "7032697924",
+  appId: "1:7032697924:web:98527c3bc7f03219d1f811",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
